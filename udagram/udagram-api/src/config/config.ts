239c650918f3ad44c1314234a4c2264b6cc4b24b
mostafa500030 <71846308+mostafa500030@users.noreply.github.com>
2022-12-10 00:45:00 +0200

    fix

--- udagram/udagram-api/src/config/config.ts
+++ udagram/udagram-api/src/config/config.ts
@@ -11,7 +11,7 @@ export const config = {
   password: process.env.POSTGRES_PASSWORD,
   database: process.env.POSTGRES_DB,
   host: process.env.POSTGRES_HOST,
-  dbport: Number(process.env.DP_PORT),
+  //dbport: Number(process.env.DP_PORT),
   aws_region: process.env.AWS_REGION,
   aws_profile: process.env.AWS_PROFILE,
   aws_media_bucket: process.env.AWS_BUCKET,
